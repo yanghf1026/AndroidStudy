ba3d7d87da7c25fb1dffc0cfa23cee7bd33634c9
yanghf <yanghf1026@thundersoft.com>
2020-11-11 11:16:02 +0800

    通过文件存储中的SQLite实现简单的词典，单词的增加和查询。

--- sqlite3/src/main/java/com/example/sqlite3/DBOpenHelper.java
+++ sqlite3/src/main/java/com/example/sqlite3/DBOpenHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
     //定义创建数据表dict的SQL语句
-    final String CREATE_TABLE_SQL = "create table dict(_id integer primary " + "key autoincrement , word ,detail)";
+    final String CREATE_TABLE_SQL = "create table dict(_id integer primary key autoincrement , word ,detail)";
 
     public DBOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version); //重写构造方法并设置工厂为null
